c6317a5b600a223fb6d6255c761840f8f7df5e81
Jbohn44 <37274622+Jbohn44@users.noreply.github.com>
2022-08-13 10:45:05 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   baseUrl: 'https://scoopreviewapi.azurewebsites.net/api/',
-  GOOGLE_PROVIDER: '83941552208-d1gms1379lt3u6lkk25jifom5kier5vv.apps.googleusercontent.com'
 };
